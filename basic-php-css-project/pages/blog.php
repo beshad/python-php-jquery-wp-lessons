d8f1e267f1f9b4b44ff45f66dd9a26c75634f687
Behshad Ghorbani <behshad.ghorbani@truescape.com>
2019-10-30 10:49:49 +1300

    minor commenting added

--- basic-php-css-project/pages/blog.php
+++ basic-php-css-project/pages/blog.php
@@ -7,14 +7,15 @@
          CURLOPT_RETURNTRANSFER => 1,
          CURLOPT_URL => 'https://jsonplaceholder.typicode.com/posts',
     ]);
-   // Send the request & save response to $resp
+   // Send the request & save response to $result
    $result = curl_exec($curl);
+   
    // decode a JSON string into an array
    $data = json_decode($result, true);
 
-   echo '<pre>';
-   print_r($data); 
-   echo "</pre><br>";
+  //  echo '<pre>';
+  //  print_r($data); 
+  //  echo "</pre><br>";
    
     // Close request to clear up some resources
     curl_close($curl);
